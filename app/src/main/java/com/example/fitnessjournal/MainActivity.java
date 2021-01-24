a6eb0273c97b325d3120e9ca06b08151c392aa62
Ryan Greenwood <RGwood002@gmail.com>
2021-01-23 22:59:11 -0800

    Added Home Button

--- app/src/main/java/com/example/fitnessjournal/MainActivity.java
+++ app/src/main/java/com/example/fitnessjournal/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.fitnessjournal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -21,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     Button convertButton;
     EditText weightField;
     TextView conversionNum;
-
+    Button home;
 
 
     @Override
@@ -34,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
          weightField = (EditText) findViewById(R.id.weightInput);
          conversionNum = (TextView) findViewById(R.id.conversionOutput);
+         home = (Button) findViewById(R.id.home_button);
 
 
         unitButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -68,7 +70,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Home.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
